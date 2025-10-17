7c8bfeb70bcb348e0dba0fe5a12227262957b542
VoiceAnime <takumi2003711@gmail.com>
2025-10-17 23:55:15 +0900

    ナビゲーションバー更新

--- components/main_layout.py
+++ components/main_layout.py
@@ -50,11 +50,10 @@ def create_navbar(user_info):
     return dbc.NavbarSimple(
         children=[
             # dbc.NavItem(dbc.NavLink("使い方", href="/howto")),
-            dbc.NavItem(dbc.NavLink("ダッシュボード", href="/")),
+            dbc.NavItem(dbc.NavLink("ダッシュボード", href="/", style={'width': '5%'})),
             dbc.NavItem(dbc.NavLink("過去問管理", href="/past-exam")),
-            dbc.NavItem(dbc.NavLink("宿題管理", href="/homework")),
+            # dbc.NavItem(dbc.NavLink("宿題管理", href="/homework")),
             dbc.NavItem(dbc.NavLink("統計", href="/statistics")), # この行を追加
-            # 不具合報告ページへのリンクを追加
             dbc.NavItem(dbc.NavLink("不具合報告", href="/bug-report")),
             dbc.NavItem(dbc.NavLink("更新履歴", href="/changelog")),
             *admin_link,
